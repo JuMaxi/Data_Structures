f0a2dddec579a4d721e7b33090b70a8f17e73f89
JuMaxi <jmaximovski@gmail.com>
2023-07-25 16:31:57 +0100

    Initial_commit

---
+++ Data_Structures_and_Algorithms/Program.cs
@@ -0,0 +1,13 @@
+﻿using Data_Structures_and_Algorithms.ADT;
+using System;
+
+namespace Data_Structures_and_Algorithms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pokemon squirtle = new Pokemon();
+        }
+    }
+}
